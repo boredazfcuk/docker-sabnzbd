dd56fb7557a05836f6833a0853af37c2ec874fb2
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-19 01:28:45 +0000

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,11 +1,11 @@
 #!/bin/ash
 
-if [ "$(nc -z "$(hostname -i)" 8080; echo "${?}")" -ne 0 ]; then
+if [ "$(nc -z "$(hostname -i)" 8080; echo $?)" -ne 0 ]; then
    echo "SABnzbd HTTP WebUI not responding on port 8080"
    exit 1
 fi
 
-if [ "$(nc -z "$(hostname -i)" 9090; echo "${?}")" -ne 0 ]; then
+if [ "$(nc -z "$(hostname -i)" 9090; echo $?)" -ne 0 ]; then
    echo "SABnzbd HTTPS WebUI not responding on port 9090"
    exit 1
 fi
