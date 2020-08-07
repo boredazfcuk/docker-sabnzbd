af4171afb8d2cbd9d8c072a30a48f4e396d8c823
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-08-07 11:33:20 +0100

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,5 +5,10 @@ if [ "$(netstat -plnt | grep -c 9090)" -ne 1 ]; then
    exit 1
 fi
 
+if [ "$(ip -o addr | grep "$(hostname -i)" | wc -l)" -eq 0 ]; then
+   echo "NIC missing"
+   exit 1
+fi
+
 echo "SABnzbd HTTP WebUIs responding OK"
 exit 0
